94df69496df4c198e03803e70b6f81861b8fae57
David Högberg <45523024+david-hogberg@users.noreply.github.com>
2025-09-25 11:15:17 +0200

    Remove hardware options

--- script.js
+++ script.js
@@ -1,47 +1,20 @@
 function setManifest() {
     const ver = document.getElementById('ver');
-    const selectedVersion = ver.options[ver.selectedIndex].text;
     const selectedOption = ver.options[ver.selectedIndex];
-    const isSPHMicrophone = document.getElementById('sph').checked;
-    
-    let manifestKey;
-    if (selectedVersion === 'WLED_0.15.0') {
-        manifestKey = `data-${isSPHMicrophone ? 'sph' : 'gma'}`;
-    } else {
-        const microphoneSuffix = isSPHMicrophone ? 'sph' : 'gma';
-        const ledmapSuffix = document.getElementById('ledmap83').checked ? '83' : '81';
-        manifestKey = `data-${microphoneSuffix}-${ledmapSuffix}`;
-    }
-
-    const manifest = selectedOption.getAttribute(manifestKey);
+    const manifest = selectedOption.getAttribute('data-manifest');
 
     if (manifest) {
         document.getElementById('inst').setAttribute('manifest', manifest);
     } else {
-        console.error('Manifest not found for the selected combination');
+        console.error('Manifest not found for the selected option');
     }
-}
-
-function toggleOptions() {
-    const ver = document.getElementById('ver');
-    const selectedVersion = ver.options[ver.selectedIndex].text;
-    const ledmapRow = document.getElementById('ledmap-row');
-
-    // Show/hide ledmap options based on version
-    ledmapRow.style.display = selectedVersion.startsWith('WLED') ? 'none' : 'block';
-
-    // Reset to default options when changing firmware
-    document.getElementById('gma').checked = true;
-    document.getElementById('ledmap83').checked = true;
-
-    setManifest();
 }
 
 function checkSupported() {
     if (document.getElementById('inst').hasAttribute('install-unsupported')) {
         unsupported();
     } else {
-        toggleOptions();
+        setManifest();
     }
 }
 
